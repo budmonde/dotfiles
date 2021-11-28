e6d8b2749c1d10ff82685940c2d950baef056a96
Monde Duinkharjav <budmonde@gmail.com>
2021-11-28 15:00:13 -0500

    Add zsh autosuggestions

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -13,3 +13,7 @@ source "$HOME/.zsh/plugins/fzf/shell/key-bindings.zsh"
 source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
+###############################################################################
+# Autosuggestions
+###############################################################################
+source ~/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
