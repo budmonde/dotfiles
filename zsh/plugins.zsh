c02d0aa04417c1422b668d702439a49b57cf8a5a
Monde Duinkharjav <budmonde@gmail.com>
2023-02-15 16:55:49 -0500

    Remove fzf from plugins

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,13 +1,4 @@
 ###############################################################################
-# Fuzzy Finder (fzf)
-###############################################################################
-# Add to fzf to path
-path_append $HOME/.shell/plugins/fzf/bin
-# Auto-completion
-[[ $- == *i* ]] && source "$HOME/.shell/plugins/fzf/shell/completion.zsh" 2> /dev/null
-# Key-binds
-source "$HOME/.shell/plugins/fzf/shell/key-bindings.zsh"
-###############################################################################
 # Syntax highlighting
 ###############################################################################
 source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
