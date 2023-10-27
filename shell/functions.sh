c782e2860aafc614f199fb81c3b50a22cbf145b6
Monde Duinkharjav <budmonde@gmail.com>
2023-10-26 20:16:57 -0700

    Add new path prepend functions

--- shell/functions.sh
+++ shell/functions.sh
@@ -11,3 +11,29 @@ path_prepend() {
     path_remove "$1"
     PATH="$1${PATH:+":$PATH"}"
 }
+manpath_remove() {
+    MANPATH=$(echo -n "$MANPATH" | awk -v RS=: -v ORS=: "\$0 != \"$1\"" | sed 's/:$//')
+}
+
+manpath_append() {
+    manpath_remove "$1"
+    MANPATH="${MANPATH:+"$MANPATH:"}$1"
+}
+
+manpath_prepend() {
+    manpath_remove "$1"
+    MANPATH="$1${MANPATH:+":$MANPATH"}"
+}
+infopath_remove() {
+    INFOPATH=$(echo -n "$INFOPATH" | awk -v RS=: -v ORS=: "\$0 != \"$1\"" | sed 's/:$//')
+}
+
+infopath_append() {
+    infopath_remove "$1"
+    INFOPATH="${INFOPATH:+"$INFOPATH:"}$1"
+}
+
+infopath_prepend() {
+    infopath_remove "$1"
+    INFOPATH="$1${INFOPATH:+":$INFOPATH"}"
+}
