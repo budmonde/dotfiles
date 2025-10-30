f1610f981d22cad87dfa2fa5db0fbaacc53ecb1a
Monde Duinkharjav <budmonde@gmail.com>
2025-10-30 09:28:11 -0700

    [SHELL] Add convenience functions for navigation

--- shell/functions.sh
+++ shell/functions.sh
@@ -37,3 +37,38 @@ infopath_prepend() {
     infopath_remove "$1"
     INFOPATH="$1${INFOPATH:+":$INFOPATH"}"
 }
+
+here() {
+    local loc
+    if [ "$#" -eq 1 ]; then
+        loc=$(realpath "$1")
+    else
+        loc=$(realpath ".")
+    fi
+    ln -sfn "${loc}" "$HOME/.shell.here"
+    echo "here -> $(readlink $HOME/.shell.here)"
+}
+
+there="$HOME/.shell.here"
+
+there() {
+    cd "$(readlink "${there}")"
+}
+
+latest_dir() {
+  local dir="${1:-.}"
+  [ -d "$dir" ] || { echo "No such directory: $dir" >&2; return 1; }
+
+  find -- "$dir" -maxdepth 1 -type d -printf '%T@ %p\0' \
+    | sort -z -nr \
+    | awk -v RS='\0' 'NR==1 { sub(/^[^ ]* /,""); print; exit }'
+}
+
+latest_file() {
+  local dir="${1:-.}"
+  [ -d "$dir" ] || { echo "No such directory: $dir" >&2; return 1; }
+
+  find -- "$dir" -maxdepth 1 -type f -printf '%T@ %p\0' \
+    | sort -z -nr \
+    | awk -v RS='\0' 'NR==1 { sub(/^[^ ]* /,""); print; exit }'
+}
