ea4147f3645ef39d86725f4a4dd4138bc8147e4b
Monde Duinkharjav <budmonde@gmail.com>
2026-02-03 14:14:51 -0500

    [SHELL] Add opencode to PATH
    Add ~/.opencode/bin to PATH in shell/bootstrap.sh.

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -1,5 +1,6 @@
 path_prepend $HOME/.local/bin:$PATH
 path_prepend $HOME/.dotfiles/bin
+path_prepend $HOME/.opencode/bin
 path_prepend $HOME/bin
 path_prepend /usr/local/bin
 path_prepend /usr/local/cuda/bin
