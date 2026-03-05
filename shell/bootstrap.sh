769f7ccb361591f56c00fd256ec9144f463b9fe2
Monde Duinkharjav <budmonde@gmail.com>
2026-03-05 16:02:38 -0500

    [SHELL] Re-order path prepends in order of priority

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -1,8 +1,15 @@
+# Lowest priority paths
+path_append $HOME/.shell/plugins/fzf/bin
+
+# System paths
+path_prepend /usr/local/cuda/bin
+path_prepend /usr/local/bin
+
+# Tool-managed paths
+path_prepend $HOME/.go/bin
+path_prepend $HOME/.opencode/bin
+
+# Personal paths
 path_prepend $HOME/.local/bin
 path_prepend $HOME/.dotfiles/bin
-path_prepend $HOME/.opencode/bin
-path_prepend $HOME/.go/bin
 path_prepend $HOME/bin
-path_prepend /usr/local/bin
-path_prepend /usr/local/cuda/bin
-path_append $HOME/.shell/plugins/fzf/bin
