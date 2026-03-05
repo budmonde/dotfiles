e509318718a7c65a213289ba4e3912b366a46a2f
Monde Duinkharjav <budmonde@gmail.com>
2026-03-05 16:00:49 -0500

    [SHELL] Add go bin path to PATH

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -1,6 +1,7 @@
 path_prepend $HOME/.local/bin
 path_prepend $HOME/.dotfiles/bin
 path_prepend $HOME/.opencode/bin
+path_prepend $HOME/.go/bin
 path_prepend $HOME/bin
 path_prepend /usr/local/bin
 path_prepend /usr/local/cuda/bin
--- shell/external.sh
+++ shell/external.sh
@@ -1,5 +1,6 @@
 export PYTHONSTARTUP=$HOME/.pythonrc
 export NVM_DIR="$HOME/.shell/plugins/nvm"
+export GOPATH="$HOME/.go"
 
 if command -v nvim &> /dev/null; then
     export EDITOR=nvim
