13b30a693540b14f122d2f25c28e4a36337fb459
Monde Duinkharjav <budmonde@gmail.com>
2026-03-24 18:13:04 -0400

    [SHELL] Import texlive script from dotfiles-local

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -1,6 +1,14 @@
 # Lowest priority paths
 path_append $HOME/.shell/plugins/fzf/bin
 
+# Texlive (dynamic platform detection)
+if [ -d "$HOME/.texlive/bin" ]; then
+    TEXLIVE_PLATFORM=$(ls "$HOME/.texlive/bin" | head -1)
+    path_append "$HOME/.texlive/bin/$TEXLIVE_PLATFORM"
+    manpath_append "$HOME/.texlive/texmf-dist/doc/man"
+    infopath_append "$HOME/.texlive/texmf-dist/doc/info"
+fi
+
 # System paths
 path_prepend /usr/local/cuda/bin
 path_prepend /usr/local/bin
