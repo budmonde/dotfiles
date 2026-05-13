9ec4c6bbb45089a7d06d482ef2586d2683bba4c9
Monde Duinkharjav <budmonde@gmail.com>
2026-05-13 18:10:08 -0400

    [SHELL] Ensure bash HISTFILE parent dir exists

--- bash/settings.bash
+++ bash/settings.bash
@@ -1,5 +1,6 @@
 # Improve History
 HISTSIZE=1048576
 HISTFILE="${XDG_STATE_HOME:-$HOME/.local/state}/bash/history"
+[ -d "${HISTFILE%/*}" ] || mkdir -p "${HISTFILE%/*}"
 SAVEHIST=$HISTSIZE
 shopt -s histappend
