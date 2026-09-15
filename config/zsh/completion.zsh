c0baba675fb57245fdb62d4eceeaa20a73a736a6
Monde Duinkharjav <budmonde@gmail.com>
2026-09-15 17:09:41 -0400

    [ZSH] Skip dangling custom completions

--- config/zsh/completion.zsh
+++ config/zsh/completion.zsh
@@ -4,10 +4,10 @@ autoload -Uz compinit && compinit -d "${XDG_CACHE_HOME:-$HOME/.cache}/zsh/zcompd
 
 # Source all custom completion definitions
 for f in ~/.config/zsh/completions/*.zsh(N); do
-    source "$f"
+    [[ -r "$f" ]] && source "$f"
 done
 
 # Source completions from external repos (e.g. dotfiles-nvidia)
 for f in ~/.config/zsh/completions.d/*.zsh(N); do
-    source "$f"
+    [[ -r "$f" ]] && source "$f"
 done
